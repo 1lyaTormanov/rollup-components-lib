354ffa72519824e35ac93ed39c4a801086ce16e4
Tarmanau <itarmanau@ibagroup.eu>
2022-07-04 06:38:18 +0300

    major features

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     testEnvironment: "jsdom",
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     moduleNameMapper: {
         ".(css|scss|sass)$": "identity-obj-proxy",
     },
